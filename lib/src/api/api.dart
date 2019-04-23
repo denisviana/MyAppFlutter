74dfe3c9336df3f421cd827fdaecf0d0fb1e5699
Denis Costa <denis.costa@snowmanlabs.com>
2019-04-23 13:22:27 -0300

    Changed auth

--- lib/src/api/api.dart
+++ lib/src/api/api.dart
@@ -7,14 +7,17 @@ import 'package:my_flutter_app/src/models/user.dart';
 
 class ApiUtil{
 
-  //Api HOST
-  final String url = "https://reqres.in/api/";
 
   Future<Response> login(User user) async {
 
+    var uname = user.email;
+    var psswd = user.password;
+
+    String url = "http://etqm.host/auth.cfm?uname=$uname&psswd=$psswd";
+
     /*Does an asynchronous http POST passing as parameter in body the email and password of user,
     the complete url is https://reqres.in/api/login*/
-    var response = await http.post(url+"login", body: {"email":user.email, "password":user.password})
+    var response = await http.get(url)
         .then((response){
 
           //Convert response string in a Map (the response is like 'key':'value')
@@ -24,11 +27,11 @@ class ApiUtil{
 
           //If key is 'error' so the request has been failed
           //If key is 'token' so the request has been successful
-          if(result.containsKey('error'))
+          if(result.containsKey('returnmessage') && result['returnmessage'] == "invalid")
             //I have created a custom entity for handle response state
-            return Response(state : HttpState.ERROR, message: result['error']);
-          else if(result.containsKey('token'))
-            return Response(state : HttpState.SUCCESSFUL, message: result['token']);
+            return Response(state : HttpState.ERROR, message: result['returnmessage']);
+          else if(result.containsKey('returnmessage') && result['returnmessage'] == "ok")
+            return Response(state : HttpState.SUCCESSFUL, message: result['Success']);
     });
 
     return response;
